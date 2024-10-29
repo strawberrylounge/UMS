113c15bb731d5df7035ab2f1b707b454c48d8076
BomiKim <93rlaqhal@gmail.com>
2024-10-29 18:41:39 +0900

    feat: 템플릿 이미지 추가 이미지 태그

--- src/components/service/TemplateCard.vue
+++ src/components/service/TemplateCard.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="template-card">
-    <div class="template-card-img"></div>
+    <div class="template-card-img">
+      <!-- NOTE: 템플릿 이미지 추가 -->
+      <!-- <img src="" alt=""> -->
+    </div>
     <slot />
   </div>
 </template>
@@ -22,6 +25,10 @@ export default {
     margin-bottom: 11px;
     background-color: var(--gray200);
     border-radius: 4px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 </style>
