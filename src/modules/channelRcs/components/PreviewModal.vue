7cba3277f76840330bffa5e5464e37ca30d2a1c7
BomiKim <93rlaqhal@gmail.com>
2024-11-16 17:01:20 +0900

    feat: #627 채널관리 미리보기 모달 흔들리는 이슈 수정

--- src/modules/channelRcs/components/PreviewModal.vue
+++ src/modules/channelRcs/components/PreviewModal.vue
@@ -1,5 +1,5 @@
 <template>  
-  <b-modal id="preview-modal" title="미리보기" hide-header-close size="s" centered content-class="preview-modal">
+  <b-modal id="preview-modal" title="미리보기" hide-header-close size="s" centered content-class="preview-modal" no-fade>
 
     <PreviewDefault>
       <div class="preview-img">
